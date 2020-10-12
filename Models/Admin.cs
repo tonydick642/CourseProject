d39d12f743d42f6654e500196b280189a4034ade
Tony Dick <tonydick642@gmail.com>
2020-10-12 18:42:58 -0500

    Added Admin Model and DbContext

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -7,5 +7,7 @@ namespace CourseProject.Models
 {
     public class Admin
     {
+        public int AdminId { get; set; }
+        public string AdminName { get; set; }
     }
 }
--- Models/SwimSchoolDbContext.cs
+++ Models/SwimSchoolDbContext.cs
@@ -9,6 +9,8 @@ namespace CourseProject.Models
     public class SwimSchoolDbContext : DbContext
     {
         public DbSet<Swimmer> Swimmers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
         public SwimSchoolDbContext(DbContextOptions
             <SwimSchoolDbContext> options) : base(options)
         { }
