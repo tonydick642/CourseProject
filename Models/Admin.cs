3331f54e07c7e687318e4bfcef927c86f0fca8c6
Tony Dick <tonydick642@gmail.com>
2020-10-12 21:56:25 -0500

    Login and Register View work

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -9,5 +9,8 @@ namespace CourseProject.Models
     {
         public int AdminId { get; set; }
         public string AdminName { get; set; }
+        public string AdminEmail { get; set; }
+        public string AdminRole { get; set; }
+        public string AdminPassword { get; set; }
     }
 }
