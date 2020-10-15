fcf850fc6e39416e7f33d0c85202601a0cc35b62
Zach Keener <zachs.infinite@gmail.com>
2020-10-14 20:41:51 -0500

    Added RoleController

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseProject.Controllers
+{
+    public class RoleController : Controller
+    {
+        SwimSchoolDbContext db;
+        UserManager<ApplicationUser> userManager;
+        RoleManager<IdentityRole> roleManager;
+        public RoleController(SwimSchoolDbContext db,
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.db = db;
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+    }
+}
