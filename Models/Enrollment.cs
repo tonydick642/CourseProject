0b5ad2f2eb966a71ddce4f84ed504f280ee7f8f1
zimzak <67248618+zimzak@users.noreply.github.com>
2020-10-23 20:37:54 -0700

    swimmer view report page

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -12,6 +12,6 @@ namespace CourseProject.Models
         public Swimmer Swimmer { get; set; }
         public int SessionId { get; set; }
         public Session Session { get; set; }
-
+        public string ProgressReport { get; set; }
     }
 }
